1a05b773852255df6b6882af99f226a7b22ca4ac
Serene <imane0897@gmail.com>
2017-04-25 22:06:38 +0800

    Multilayer Convolutional Network

--- char_reg.py
+++ char_reg.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 from tensorflow.python.framework import graph_util
-from tensorflow.examples.tutorials.mnist import input_data
 
-# --- Build a Multilayer Convolutional Network ---
 
 # Weight Initialization
 def weight_variable(shape):
@@ -24,7 +22,7 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 
-def main():
+def main(test_data):
 
     # Placeholders
     x = tf.placeholder(tf.float32, shape=[None, 784])
@@ -67,41 +65,19 @@ def main():
     b_fc2 = bias_variable([10])
 
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    y = tf.nn.softmax(y_conv)
 
 
-    # Train and Evaluate the Model
-    cross_entropy = tf.reduce_mean(
-                        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-
-    sess = tf.InteractiveSession()
-    sess.run(tf.global_variables_initializer())
-
-
-    # Train model
-    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
-    for i in range(20000):
-        batch = mnist.train.next_batch(50)
-        if i % 100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-            print("step %d, training accuracy %g"%(i, train_accuracy))
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-    print("test accuracy %g"%accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-
-
-    # Save model
+    # Restore model
     saver = tf.train.Saver({"W_conv1": W_conv1, "b_conv1": b_conv1,
                             "W_conv2": W_conv2, "b_conv2": b_conv2,
                             "W_fc1": W_fc1, "b_fc1": b_fc1,
                             "W_fc2": W_fc2, "b_fc2": b_fc2})
-    save_path = saver.save(sess, "/tmp/model.ckpt")
-    print("Model saved in file: %s" % save_path)
+
+    with tf.Session() as sess:
+        saver.restore(sess, "/Users/AnYameng/Documents/OCR/mnist/model.ckpt")
+        prediction = sess.run(y, feed_dict={x: test_data, keep_prob: 1.0})
+        print(tf.argmax(prediction, 1).eval())
 
 
 if __name__ == '__main__':
